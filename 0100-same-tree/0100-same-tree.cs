007a94615f12a137235e5f67932b276776fa952a
Abdelrahman Abdelmougeth <abdelrahmanabdelmougeth@gmail.com>
2024-12-26 23:20:55 +0200

    Time: 0 ms (100.00%), Space: 42.6 MB (46.59%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true; // Both are null 
+        if (p == null || q == null) return false; // One is null, the other is not if (p.val 
+        if(p.val != q.val) return false; // values are not the same
+        
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        
+        return true;
+    }
+}
