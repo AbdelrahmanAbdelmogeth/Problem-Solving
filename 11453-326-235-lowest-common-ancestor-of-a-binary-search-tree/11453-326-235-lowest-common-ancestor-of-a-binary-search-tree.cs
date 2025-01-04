2c85033125cc050c5e8c8dc971cadb992999756b
Abdelrahman Abdelmougeth <abdelrahmanabdelmougeth@gmail.com>
2025-01-04 23:09:36 +0200

    Time: 106 ms, Memory: 49.4 MB - LeetHub

--- 11453-326-235-lowest-common-ancestor-of-a-binary-search-tree/11453-326-235-lowest-common-ancestor-of-a-binary-search-tree.cs
+++ 11453-326-235-lowest-common-ancestor-of-a-binary-search-tree/11453-326-235-lowest-common-ancestor-of-a-binary-search-tree.cs
@@ -25,13 +25,26 @@ public class Solution {
         return pNodePath[i - 1]; // Return the last common ancestor
     }
     
-    private bool FindPath(TreeNode root, TreeNode node, List<TreeNode> NodePath){
-        if(root == null) return false;
-        NodePath.Add(root);
-        if(root.val == node.val) return true;
-        if(FindPath(root.left, node, NodePath) || FindPath(root.right, node, NodePath))
-            return true;
-        NodePath.RemoveAt(NodePath.Count - 1);
-        return false;
+    private bool FindPath(TreeNode root, TreeNode node, List<TreeNode> NodePath) {
+        if (root == null) return false; // Base Case – Check if the current node is null
+        
+        NodePath.Add(root); // Add the current node (root) to the NodePath list. Assume the current node is a part of the node's path
+        
+        if (root.val == node.val) return true; // Check if the current node is the target node retrun true indicating a success found
+
+        if (node.val < root.val) {
+            // If the target node's value is smaller, search the left subtree
+            if (FindPath(root.left, node, NodePath))
+                return true;
+        } else {
+            // If the target node's value is greater, search the right subtree
+            if (FindPath(root.right, node, NodePath))
+                return true;
+        }
+
+        NodePath.RemoveAt(NodePath.Count - 1); // Backtracking – Remove the current node if it's not part of the path since neither subtree found the target node, the current node is not on the path to the target node.
+        return false; // Return false to indicate failure and allow the previous recursive call to continue its search.
     }
+    
+
 }
