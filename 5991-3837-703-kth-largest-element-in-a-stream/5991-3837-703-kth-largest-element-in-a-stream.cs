c0ea4f1d073ce53f5ce327a737f8442a4eb53d41
Abdelrahman Abdelmougeth <abdelrahmanabdelmougeth@gmail.com>
2025-01-30 12:57:46 +0200

    Time: 26 ms, Memory: 68.3 MB - LeetHub

--- 5991-3837-703-kth-largest-element-in-a-stream/5991-3837-703-kth-largest-element-in-a-stream.cs
+++ 5991-3837-703-kth-largest-element-in-a-stream/5991-3837-703-kth-largest-element-in-a-stream.cs
@@ -1,20 +1,20 @@
 public class KthLargest {
-    private PriorityQueue<int, int> minHeap;
-    int k;
-
+    int k; // to store the kth Largest
+    private PriorityQueue<int, int> minHeap; // mentain a min heap to always mentain the lowest value
+    
     public KthLargest(int k, int[] nums) {
-        minHeap = new PriorityQueue<int, int>();
-        this.k = k;
+        minHeap = new PriorityQueue<int, int>(); // initialize the min heap
+        this.k = k; // initialize the kth largest to keep track of it
 
-        foreach (var num in nums)
+        foreach (var num in nums) // construct the min heap
             Add(num);
     }
     
     public int Add(int val) {
-        minHeap.Enqueue(val, val);
-        if(minHeap.Count > this.k)
+        minHeap.Enqueue(val, val); // enqueue the value with priority val (if we want max heap we use -ve in priority)
+        if(minHeap.Count > this.k) // if the number of elements in the queue excceeds the k we remove one that's make sure we mentain only the kth largest in the peek of the queue
             minHeap.Dequeue();
-        return  minHeap.Peek();     
+        return  minHeap.Peek(); // always there is the kth largest (the lowest value) in the peek of the queue     
     }
 }
 
